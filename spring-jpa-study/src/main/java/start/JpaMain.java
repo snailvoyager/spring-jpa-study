6ca05970315a0a53ab8a622918ff658aa85a46e8
snailvoyager <kyj88514@gmail.com>
2020-05-04 19:56:32 +0900

    객체매핑, 간단 로직

--- spring-jpa-study/src/main/java/start/JpaMain.java
+++ spring-jpa-study/src/main/java/start/JpaMain.java
@@ -1,5 +1,48 @@
 package start;
 
+import java.util.List;
+
+import javax.persistence.EntityManager;
+import javax.persistence.EntityManagerFactory;
+import javax.persistence.EntityTransaction;
+import javax.persistence.Persistence;
+
 public class JpaMain {
 
+	public static void main(String[] args) {
+		EntityManagerFactory emf = Persistence.createEntityManagerFactory("jpabook");	//엔티티 매니저 팩토리 생성
+		EntityManager em = emf.createEntityManager();		//엔티티 매니저 생성
+		EntityTransaction tx = em.getTransaction();			//트랜잭션 획득
+		
+		try {
+			tx.begin();
+			logic(em);			//비지니스 로직
+			tx.commit();
+		} catch(Exception e) {
+			tx.rollback();
+		} finally {
+			em.close();
+		}
+		emf.close();
+	}
+	
+	private static void logic(EntityManager em) {
+		String id = "id1";
+		Member member = new Member();
+		member.setId(id);
+		member.setUsername("AA");
+		member.setAge(2);
+		
+		em.persist(member);		//등록
+		
+		member.setAge(20);		//수정
+		
+		Member findMember = em.find(Member.class, id);		//한건 조회
+		System.out.println("findMember=" + findMember.getUsername() + ", age=" + findMember.getAge());
+		
+		List<Member> members = em.createQuery("select m from Member m", Member.class).getResultList();	//목록 조회
+		System.out.println("members.size=" + members.size());
+		
+		em.remove(members);		//삭제
+	}
 }
--- spring-jpa-study/src/main/java/start/Member.java
+++ spring-jpa-study/src/main/java/start/Member.java
@@ -1,5 +1,38 @@
 package start;
 
-public class Member {
+import javax.persistence.Column;
+import javax.persistence.Entity;
+import javax.persistence.Id;
+import javax.persistence.Table;
 
+@Entity
+@Table(name="MEMBER")
+public class Member {
+	@Id			//PrimaryKey에 매핑 식필자 필드
+	@Column(name="ID")
+	private String id;
+	@Column(name="NAME")
+	private String username;
+	
+	private Integer age;	//매핑정보가 없어도 필드명을 사용해서 컬럼명에 매핑
+	
+	public String getId() {
+		return id;
+	}
+	public void setId(String id) {
+		this.id = id;
+	}
+	public String getUsername() {
+		return username;
+	}
+	public void setUsername(String username) {
+		this.username = username;
+	}
+	public Integer getAge() {
+		return age;
+	}
+	public void setAge(Integer age) {
+		this.age = age;
+	}
+	
 }
